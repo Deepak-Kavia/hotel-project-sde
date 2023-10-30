45ef660e98e679380531496caf0a1014bd9d2eea
Deepak Singh <deepkavia2001@gmail.com>
2023-10-30 18:16:46 +0530

    hotel management project login issue

---
+++ backend-main/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const path = require('path');
+const Port = process.env.PORT || 8000;
+require('dotenv').config()
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO);
+mongoose.connection.on('connected',() =>{
+    console.log("connected to mongo");
+})
+
+require("./models/Hotel")
+require("./models/Room")
+require("./models/CountHotel")
+require("./models/HotelCateory")
+require("./models/User")
+
+// app.get("/",(req, res)=>{
+//  res.json({"data":"home"})
+//  })
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser())
+
+app.use(require("./routes/hotel"))
+app.use(require("./routes/auth"))
+app.use(require("./routes/user"))
+app.use(require("./routes/room"))
+app.use(express.json());
+
+
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static('client/build'));
+
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+  })
+}
+
+app.listen(Port, () => {
+  console.log('server is running on port 8000 . . .');
+})
---
+++ backend-main/routes/hotel.js
@@ -0,0 +1,42 @@
+const {getAllHotel,createCategory,createHotel,updateHotel,deleteHotel,getHotel,cityHotel,getByFeature,getByCategory,countByCity,getHotelRooms,createCity,updateCityCount,getAllCity,getAllCategoryCity,getHotelByFilter} = require('../controllers/hotel');
+const {verifyAdmin} = require('../utlis/verifyToken');
+const express = require("express")
+const router = express.Router();
+
+
+//UpdateCityCount
+router.put("/updateCityCount", updateCityCount)
+//CREATE
+router.post("/hotel/createhotel",verifyAdmin,createHotel)
+//UPDATE
+router.put("/hotel/:id",verifyAdmin,updateHotel)
+//DELETE
+router.delete("/hotel/:id",verifyAdmin, deleteHotel)
+//GET
+router.get("/hotel/find/:id", getHotel)
+//GET
+router.get("/hotel/cityhotel/:id", cityHotel)
+//GET ALL
+router.get("/", getAllHotel);
+//GET HOTEL BY FILTER
+router.get("/hotel/getHotelByFilter", getHotelByFilter);
+//GetByCity
+router.get("/hotel/getByFeature",getByFeature)
+//GetByCategory
+router.get("/hotel/getByCategory",getByCategory)
+//CountByCity
+router.get("/hotel/countByCity", countByCity)
+//CreateCity
+router.post("/hotel/createCity", createCity)
+//GetAllCity
+router.get("/getAllCity",getAllCity)
+//getRoom
+router.get("/hotel/room/:id", getHotelRooms)
+//CreateCat
+router.post("/hotel/createCategory", createCategory)
+//GetAllCat
+router.get("/hotel/getAllCategoryCity",getAllCategoryCity)
+
+
+
+module.exports = router
